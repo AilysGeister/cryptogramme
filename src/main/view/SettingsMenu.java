519e0e50f56059ccc475a3adfc70e8abb5ebf7b7
AilysGeister <samantha.guivret@gmail.com>
2026-01-13 15:32:24 +0100

    Update SettingsMenu.java

--- src/main/view/SettingsMenu.java
+++ src/main/view/SettingsMenu.java
@@ -16,14 +16,16 @@ import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class SettingsMenu extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private MainFrame mainFrame;
-	private JTextField txtDefaultExportLocation;
-	private JTextField txtDefaultkeysLocation;
+	public JTextField txtDefaultExportLocation, txtDefaultkeysLocation;
+	public JLabel returnMsgJLabel;
 
 	/**
 	 * Create the panel.
@@ -31,42 +33,29 @@ public class SettingsMenu extends JPanel {
 	 */
 	public SettingsMenu(MainFrame mainFrame) throws IOException {
 		this.mainFrame = mainFrame;
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
+		setLayout(new BorderLayout(0, 0));
 		
-		BackJButton backButton = new BackJButton(mainFrame);
-		backButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mainFrame.showPage("TitleScreen");
-			}
-		});
-		GridBagConstraints gbc_backButton = new GridBagConstraints();
-		gbc_backButton.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_backButton.insets = new Insets(0, 0, 5, 5);
-		gbc_backButton.gridx = 0;
-		gbc_backButton.gridy = 0;
-		add(backButton, gbc_backButton);
+		JPanel footerJPanel = new JPanel();
+		add(footerJPanel, BorderLayout.SOUTH);
 		
-		JLabel settingsLabel = new JLabel("Settings");
-		settingsLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		GridBagConstraints gbc_settingsLabel = new GridBagConstraints();
-		gbc_settingsLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_settingsLabel.gridx = 0;
-		gbc_settingsLabel.gridy = 1;
-		add(settingsLabel, gbc_settingsLabel);
+		this.returnMsgJLabel = new JLabel();
+		returnMsgJLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		footerJPanel.add(returnMsgJLabel);
+		
+		JPanel bodyJPanel = new JPanel();
+		add(bodyJPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_bodyJPanel = new GridBagLayout();
+		gbl_bodyJPanel.columnWeights = new double[]{0.0, 1.0, 0.0};
+		gbl_bodyJPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+		bodyJPanel.setLayout(gbl_bodyJPanel);
 		
 		JLabel localizationLabel = new JLabel("Language:");
 		localizationLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		GridBagConstraints gbc_localizationLabel = new GridBagConstraints();
 		gbc_localizationLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_localizationLabel.anchor = GridBagConstraints.BASELINE_LEADING;
 		gbc_localizationLabel.gridx = 0;
-		gbc_localizationLabel.gridy = 2;
-		add(localizationLabel, gbc_localizationLabel);
+		gbc_localizationLabel.gridy = 0;
+		bodyJPanel.add(localizationLabel, gbc_localizationLabel);
 		
 		JComboBox<ComboItem> languageComboBox = new JComboBox<ComboItem>();
 		languageComboBox.addItem(new ComboItem(PropertiesReader.getString("english"), "en"));
@@ -76,52 +65,75 @@ public class SettingsMenu extends JPanel {
 		gbc_languageComboBox.anchor = GridBagConstraints.WEST;
 		gbc_languageComboBox.insets = new Insets(0, 0, 5, 5);
 		gbc_languageComboBox.gridx = 1;
-		gbc_languageComboBox.gridy = 2;
-		add(languageComboBox, gbc_languageComboBox);
+		gbc_languageComboBox.gridy = 0;
+		bodyJPanel.add(languageComboBox, gbc_languageComboBox);
 		
 		JLabel defaultExportLocationLabel = new JLabel("Default export location:");
 		defaultExportLocationLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		GridBagConstraints gbc_defaultExportLocationLabel = new GridBagConstraints();
-		gbc_defaultExportLocationLabel.anchor = GridBagConstraints.EAST;
 		gbc_defaultExportLocationLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_defaultExportLocationLabel.gridx = 0;
-		gbc_defaultExportLocationLabel.gridy = 3;
-		add(defaultExportLocationLabel, gbc_defaultExportLocationLabel);
+		gbc_defaultExportLocationLabel.gridy = 1;
+		bodyJPanel.add(defaultExportLocationLabel, gbc_defaultExportLocationLabel);
 		txtDefaultExportLocation = new JTextField();
 		txtDefaultExportLocation.setText(Settings.getStrParameter("export_directory"));
 		GridBagConstraints gbc_txtDefaultExportLocation = new GridBagConstraints();
 		gbc_txtDefaultExportLocation.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtDefaultExportLocation.insets = new Insets(0, 0, 5, 5);
 		gbc_txtDefaultExportLocation.gridx = 1;
-		gbc_txtDefaultExportLocation.gridy = 3;
-		add(txtDefaultExportLocation, gbc_txtDefaultExportLocation);
+		gbc_txtDefaultExportLocation.gridy = 1;
+		bodyJPanel.add(txtDefaultExportLocation, gbc_txtDefaultExportLocation);
 		txtDefaultExportLocation.setColumns(10);
 		
-		DirectoryChooserJButton exporyLocationDirectoryChooserJButton_1 = new DirectoryChooserJButton(txtDefaultExportLocation);
-		GridBagConstraints gbc_exporyLocationDirectoryChooserJButton_1 = new GridBagConstraints();
-		gbc_exporyLocationDirectoryChooserJButton_1.fill = GridBagConstraints.BOTH;
-		gbc_exporyLocationDirectoryChooserJButton_1.insets = new Insets(0, 0, 5, 0);
-		gbc_exporyLocationDirectoryChooserJButton_1.gridx = 2;
-		gbc_exporyLocationDirectoryChooserJButton_1.gridy = 3;
-		add(exporyLocationDirectoryChooserJButton_1, gbc_exporyLocationDirectoryChooserJButton_1);
+		DirectoryChooserJButton exportLocationDirectoryChooserJButton_1 = new DirectoryChooserJButton(txtDefaultExportLocation);
+		GridBagConstraints gbc_exportLocationDirectoryChooserJButton_1 = new GridBagConstraints();
+		gbc_exportLocationDirectoryChooserJButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_exportLocationDirectoryChooserJButton_1.gridx = 2;
+		gbc_exportLocationDirectoryChooserJButton_1.gridy = 1;
+		bodyJPanel.add(exportLocationDirectoryChooserJButton_1, gbc_exportLocationDirectoryChooserJButton_1);
 		
 		JLabel defaultKeysLocationLabel = new JLabel("Default keys location:");
 		defaultKeysLocationLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		GridBagConstraints gbc_defaultKeysLocationLabel = new GridBagConstraints();
-		gbc_defaultKeysLocationLabel.anchor = GridBagConstraints.WEST;
 		gbc_defaultKeysLocationLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_defaultKeysLocationLabel.gridx = 0;
-		gbc_defaultKeysLocationLabel.gridy = 4;
-		add(defaultKeysLocationLabel, gbc_defaultKeysLocationLabel);
-		JButton saveButton = new JButton("Save");
-		saveButton.addActionListener(new ActionListener() {
+		gbc_defaultKeysLocationLabel.gridy = 2;
+		bodyJPanel.add(defaultKeysLocationLabel, gbc_defaultKeysLocationLabel);
+		
+		txtDefaultkeysLocation = new JTextField();
+		txtDefaultkeysLocation.setText(Settings.getStrParameter("keys_directory"));
+		GridBagConstraints gbc_txtDefaultkeysLocation = new GridBagConstraints();
+		gbc_txtDefaultkeysLocation.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtDefaultkeysLocation.insets = new Insets(0, 0, 5, 5);
+		gbc_txtDefaultkeysLocation.gridx = 1;
+		gbc_txtDefaultkeysLocation.gridy = 2;
+		bodyJPanel.add(txtDefaultkeysLocation, gbc_txtDefaultkeysLocation);
+		txtDefaultkeysLocation.setColumns(10);
+		
+		DirectoryChooserJButton keyLocationDirectoryChooserJButton = new DirectoryChooserJButton(txtDefaultkeysLocation);
+		GridBagConstraints gbc_keyLocationDirectoryChooserJButton = new GridBagConstraints();
+		gbc_keyLocationDirectoryChooserJButton.insets = new Insets(0, 0, 5, 0);
+		gbc_keyLocationDirectoryChooserJButton.gridx = 2;
+		gbc_keyLocationDirectoryChooserJButton.gridy = 2;
+		bodyJPanel.add(keyLocationDirectoryChooserJButton, gbc_keyLocationDirectoryChooserJButton);
+		
+		JButton saveJButton = new JButton(PropertiesReader.getString("save"));
+		GridBagConstraints gbc_saveJButton = new GridBagConstraints();
+		gbc_saveJButton.insets = new Insets(0, 0, 5, 5);
+		gbc_saveJButton.gridx = 0;
+		gbc_saveJButton.gridy = 3;
+		bodyJPanel.add(saveJButton, gbc_saveJButton);
+		saveJButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					if (!txtDefaultExportLocation.getText().isBlank() || !txtDefaultkeysLocation.getText().isBlank()) {
 						ComboItem selected = (ComboItem) languageComboBox.getSelectedItem();
 						Settings.updateSettings(true, selected.getStrValue(), txtDefaultExportLocation.getText(), txtDefaultkeysLocation.getText());
+						returnMsgJLabel.setForeground(new Color(0, 175, 0));
+						returnMsgJLabel.setText(PropertiesReader.getString("saveSettingsSuccess"));
 					} else {
-						new ErrorDialogBox();
+						returnMsgJLabel.setForeground(new Color(175, 0, 0));
+						returnMsgJLabel.setText(PropertiesReader.getString("saveSettingsFaile"));
 					}
 				} catch (IOException e1) {
 					e1.printStackTrace();
@@ -129,36 +141,28 @@ public class SettingsMenu extends JPanel {
 			}
 		});
 		
-		txtDefaultkeysLocation = new JTextField();
-		txtDefaultkeysLocation.setText(Settings.getStrParameter("keys_directory"));
-		GridBagConstraints gbc_txtDefaultkeysLocation = new GridBagConstraints();
-		gbc_txtDefaultkeysLocation.insets = new Insets(0, 0, 5, 5);
-		gbc_txtDefaultkeysLocation.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtDefaultkeysLocation.gridx = 1;
-		gbc_txtDefaultkeysLocation.gridy = 4;
-		add(txtDefaultkeysLocation, gbc_txtDefaultkeysLocation);
-		txtDefaultkeysLocation.setColumns(10);
-		
-		DirectoryChooserJButton keyLocationDirectoryChooserJButton = new DirectoryChooserJButton(txtDefaultkeysLocation);
-		GridBagConstraints gbc_keyLocationDirectoryChooserJButton = new GridBagConstraints();
-		gbc_keyLocationDirectoryChooserJButton.fill = GridBagConstraints.BOTH;
-		gbc_keyLocationDirectoryChooserJButton.insets = new Insets(0, 0, 5, 0);
-		gbc_keyLocationDirectoryChooserJButton.gridx = 2;
-		gbc_keyLocationDirectoryChooserJButton.gridy = 4;
-		add(keyLocationDirectoryChooserJButton, gbc_keyLocationDirectoryChooserJButton);
-		
 		JLabel warning = new JLabel(PropertiesReader.getString("warningSettings"));
-		warning.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		GridBagConstraints gbc_warning = new GridBagConstraints();
-		gbc_warning.insets = new Insets(0, 0, 5, 5);
+		gbc_warning.insets = new Insets(0, 0, 0, 5);
 		gbc_warning.gridx = 1;
-		gbc_warning.gridy = 5;
-		add(warning, gbc_warning);
-		GridBagConstraints gbc_saveButton = new GridBagConstraints();
-		gbc_saveButton.insets = new Insets(0, 0, 0, 5);
-		gbc_saveButton.gridx = 0;
-		gbc_saveButton.gridy = 6;
-		add(saveButton, gbc_saveButton);
+		gbc_warning.gridy = 4;
+		bodyJPanel.add(warning, gbc_warning);
+		warning.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		
+		JPanel headerJPanel = new JPanel();
+		add(headerJPanel, BorderLayout.NORTH);
+		
+		BackJButton backButton = new BackJButton(mainFrame);
+		headerJPanel.add(backButton);
+		backButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.showPage("TitleScreen");
+			}
+		});
+		
+		JLabel settingsLabel = new JLabel(PropertiesReader.getString("settings"));
+		headerJPanel.add(settingsLabel);
+		settingsLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 	}
 
 }
